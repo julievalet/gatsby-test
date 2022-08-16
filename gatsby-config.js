806e21ceff08d34aa9a0751681e82ea03fa459a9
Julie Valet <jvalet@powergo.ca>
2022-08-16 14:12:09 -0400

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  siteMetadata: {
-    title: `Web 3.0 Test`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [],
-}
+    siteMetadata: {
+        title: `Web 3.0 Test`,
+        siteUrl: `https://www.yourdomain.tld`
+    },
+    plugins: [
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: `blog`,
+                path: `${__dirname}/blog`
+            }
+        }
+    ]
+};
